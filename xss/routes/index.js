8d1e86a23f54aae7bf6a4b3e69bb10f3dfe8378d
GEMDALE\huangenyong <gemdale.com@huangenyong>
2019-01-25 09:56:26 +0800

    存储型XSS

--- xss/routes/index.js
+++ xss/routes/index.js
@@ -7,5 +7,13 @@ router.get('/', function(req, res, next) {
 	res.set('X-XSS-Protection',0);
   	res.render('index', { title: 'Express',xss:req.query.xss });
 });
+/*
+* 存储型XSS
+router.get('/aaa', function(req, res, next) {
+	//关掉浏览器xss拦截
+	res.set('X-XSS-Protection',0);
+  	res.render('index', { title: 'Express',xss:sql() }); //从缓存或者数据库等读取
+});
+*/
 
 module.exports = router;
